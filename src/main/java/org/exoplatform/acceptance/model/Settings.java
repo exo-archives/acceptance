1e9d4f7af43272afca01209b5b7b8410ccb9cba3
Arnaud Héritier <aheritier@exoplatform.com>
2013-09-24 10:08:10 +0200

    [ACC-11] Settings => Singleton

--- src/main/java/org/exoplatform/acceptance/model/Settings.java
+++ src/main/java/org/exoplatform/acceptance/model/Settings.java
@@ -1,6 +1,7 @@
 package org.exoplatform.acceptance.model;
 
 import javax.inject.Named;
+import javax.inject.Singleton;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 
@@ -8,10 +9,10 @@ import org.springframework.beans.factory.annotation.Value;
  * Acceptance Settings
  */
 @Named("settings")
+@Singleton
 @Data
 public class Settings {
 
   @Value("acceptance.version")
   private String version;
-
 }
